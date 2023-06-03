b7f14aff3fd2a68b1bfa99fa299db6c3c7065278
weijiepris <altxmare@hotmail.com>
2023-06-03 20:54:51 +0800

    -

--- src/redux/sagas/requests/employee.js
+++ src/redux/sagas/requests/employee.js
@@ -1,8 +1,20 @@
 import axios from "axios";
 
 export function requestGetEmployee() {
+  axios
+    .request({
+      method: "get",
+      url: "http://localhost:3001/",
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   return axios.request({
     method: "get",
-    url: "http://backend:3001/employees",
+    url: "http://localhost:3001/employees",
   });
 }
